b91e17750366d8be992473085cc10e1b887fd5ba
megan <meggsnbacon777@gmail.com>
2020-08-14 23:35:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do | x|
-   valid_calls<=>planeteer_calls
+  if valid_calls<=>planeteer_calls
  puts x 
      
   end 
